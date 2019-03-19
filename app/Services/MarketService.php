ac60593753720fca51d25c766db89681d750c7ce
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-03-19 16:08:56 -0500

    Implementando el método para detectar respuestas de error

--- app/Services/MarketService.php
+++ app/Services/MarketService.php
@@ -52,7 +52,9 @@ class MarketService
      */
     public function checkIfErrorResponse($response)
     {
-        //
+        if (isset($response->error)) {
+            throw new \Exception("Something failed: {$response->error}");
+        }
     }
 
     public function resolveAccessToken()
