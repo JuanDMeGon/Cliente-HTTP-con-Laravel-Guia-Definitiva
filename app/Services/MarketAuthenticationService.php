70d846d33663446d9458fae94f1340ecff2805bc
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-03-21 15:32:09 -0500

    Renovando tokens expirados de usuarios

--- app/Services/MarketAuthenticationService.php
+++ app/Services/MarketAuthenticationService.php
@@ -138,6 +138,42 @@ class MarketAuthenticationService
     {
         $user = auth()->user();
 
+        if (now()->lt($user->token_expires_at)) {
+            return $user->access_token;
+        }
+
+        return $this->refreshAuthenticatedUserToken($user);
+    }
+
+    public function refreshAuthenticatedUserToken($user)
+    {
+        $clientId = $this->clientId;
+        $clientSecret = $this->clientSecret;
+
+        if ($user->grant_type === 'password') {
+            $clientId = $this->passwordClientId;
+            $clientSecret = $this->passwordClientSecret;
+        }
+
+        $formParams = [
+            'grant_type' => 'refresh_token',
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'refresh_token' => $user->refresh_token,
+        ];
+
+        $tokenData = $this->makeRequest('POST', 'oauth/token', [], $formParams);
+
+        $this->storeValidToken($tokenData, $user->grant_type);
+
+        $user->fill([
+            'access_token' => $tokenData->access_token,
+            'refresh_token' => $tokenData->refresh_token,
+            'token_expires_at' => $tokenData->token_expires_at,
+        ]);
+
+        $user->save();
+
         return $user->access_token;
     }
 
