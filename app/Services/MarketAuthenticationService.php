972a5b95067c6cfa5459551c96da35ae4255f82e
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-03-21 00:10:42 -0500

    Obteniendo un token válido a partir de las credenciales de un usuario

--- app/Services/MarketAuthenticationService.php
+++ app/Services/MarketAuthenticationService.php
@@ -107,6 +107,30 @@ class MarketAuthenticationService
     }
 
     /**
+     * Obtains an access token from user credentials
+     * @param  string $username
+     * @param  string $password
+     * @return stdClass
+     */
+    public function getPasswordToken($username, $password)
+    {
+        $formParams = [
+            'grant_type' => 'password',
+            'client_id' => $this->passwordClientId,
+            'client_secret' => $this->passwordClientSecret,
+            'username' => $user,
+            'password' => $password,
+            'scope' => 'purchase-product manage-products manage-account read-general',
+        ];
+
+        $tokenData = $this->makeRequest('POST', 'oauth/token', [], $formParams);
+
+        $this->storeValidToken($tokenData, 'password');
+
+        return $tokenData;
+    }
+
+    /**
      * Stores a valid token
      * @param  stdClass $tokenData
      * @param  string $grantType
