426b5ce1afeb86a3398522a79d61979e94d563dc
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-03-26 15:16:56 -0500

    Manejando excepciones internas del cliente HTTP

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <h2 class="display-2">500 Error</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p class="lead">We did not expext this. Try again later.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
