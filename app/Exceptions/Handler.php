0f4f6dc3be8ff34d4a0eb09431b2e60ab51e7b5b
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-03-26 13:52:35 -0500

    Preparando el cliente HTTP para controlar fallas en las peticiones

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +47,27 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ClientException) {
+            return $this->handleClientException($exception, $request);
+        }
         return parent::render($request, $exception);
+    }
+
+    protected function handleClientException(ClientException $exception, $request)
+    {
+        $code = $exception->getCode();
+
+        $response = json_decode($exception->getBody()->getContents());
+        $errorMessage = $response->error;
+
+        switch ($code) {
+            case 'value':
+                # code...
+                break;
+
+            default:
+                # code...
+                break;
+        }
     }
 }
